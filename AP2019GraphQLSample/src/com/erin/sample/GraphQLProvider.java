6bf811213bb79e6550cb6449ac26eb43eef36706
Ya Peng Dong <dypdong@cn.ibm.com>
2019-07-24 23:50:07 +0800

    test

--- AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
+++ AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
@@ -43,8 +43,6 @@ public class GraphQLProvider {
         return RuntimeWiring.newRuntimeWiring()
                 .type(newTypeWiring("Query")
                         .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
-                .type(newTypeWiring("Book")
-                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
                 .build();
     }
 
