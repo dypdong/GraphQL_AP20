61c6f1e834977cb5b2d0516bb53dd3297c931f28
Ya Peng Dong <dypdong@cn.ibm.com>
2019-09-11 14:35:27 +0800

    Update GraphQLProvider.java

--- AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
+++ AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.annotation.PostConstruct;
+
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
 @Component
@@ -26,25 +28,32 @@ public class GraphQLProvider {
     GraphQLDataFetchers graphQLDataFetchers;
     
     
-    @SuppressWarnings("unused")
-	private GraphQL graphQL;
+    private GraphQL graphQL;
 
     @Bean
-    public GraphQL graphQL() throws IOException { 
+    public GraphQL graphQL() { 
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
         URL url = Resources.getResource("schema.graphqls");
         String sdl = Resources.toString(url, Charsets.UTF_8);
         GraphQLSchema graphQLSchema = buildSchema(sdl);
-        return this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        System.out.print("dypfong");
+        System.out.print(GraphQL.newGraphQL(graphQLSchema).build().getClass().getName());
     }
 
-//    @Bean
-//    public GraphQLSchema getGraphQLSchema() throws IOException {
-//        // Read the schema
-//    	System.out.println("chushihua");
-//    	URL url = Resources.getResource("schema.graphqls");
-//        String sdl = Resources.toString(url, Charsets.UTF_8);
-//        return buildSchema(sdl);
-//    }
+    @Bean
+    public GraphQLSchema getGraphQLSchema() throws IOException {
+        // Read the schema
+    	URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        System.out.print("dypdong");
+        System.out.print(buildSchema(sdl).getClass().getName());
+        return buildSchema(sdl);
+    }
     
     private GraphQLSchema buildSchema(String sdl) {
         //initial Schema
