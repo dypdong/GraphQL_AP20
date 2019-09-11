0157966346be50a0cdb39876db32218257aceeba
Ya Peng Dong <dypdong@cn.ibm.com>
2019-09-11 11:22:29 +0800

    Update GraphQLProvider.java

--- AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
+++ AP2019GraphQLSample/src/com/erin/sample/GraphQLProvider.java
@@ -2,6 +2,8 @@ package com.erin.sample;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+
+import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.annotation.PostConstruct;
+
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
 @Component
@@ -22,15 +26,31 @@ public class GraphQLProvider {
 
     @Autowired
     GraphQLDataFetchers graphQLDataFetchers;
+    
+    
+    private GraphQL graphQL;
 
     @Bean
-    public GraphQLSchema getGraphQLSchema() throws IOException {
-        // Read the schema
-    	URL url = Resources.getResource("schema.graphqls");
-        String sdl = Resources.toString(url, Charsets.UTF_8);
-        return buildSchema(sdl);
+    public GraphQL graphQL() { 
+        return graphQL;
     }
 
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+//    @Bean
+//    public GraphQLSchema getGraphQLSchema() throws IOException {
+//        // Read the schema
+//    	URL url = Resources.getResource("schema.graphqls");
+//        String sdl = Resources.toString(url, Charsets.UTF_8);
+//        return buildSchema(sdl);
+//    }
+    
     private GraphQLSchema buildSchema(String sdl) {
         //initial Schema
         TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
